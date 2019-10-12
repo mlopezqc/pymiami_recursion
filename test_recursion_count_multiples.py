2d56fb376578700424a5bc41464c61e322fcc951
Mauricio Lopez <mlopezqc@gmail.com>
2019-10-12 12:56:17 -0400

    Fixes import and changes testcases names

--- test_recursion_count_multiples.py
+++ test_recursion_count_multiples.py
@@ -1,13 +1,13 @@
 import unittest
-from exercise import count_multiples
+from exercise_mlopezqc import count_multiples
 
 
 class CountMultiples(unittest.TestCase):
-    def test_count_4(self):
+    def test_count_1(self):
         self.assertEquals(count_multiples(2, 4), 2)
 
-    def test_count_12(self):
+    def test_count_2(self):
         self.assertEquals(count_multiples(2, 12), 2)
 
-    def test_count_11664(self):
+    def test_count_3(self):
         self.assertEquals(count_multiples(3, 11664), 6)
